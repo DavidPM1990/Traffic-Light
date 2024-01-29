966da5b0906495c48ec65e958a810cbe6c89e394
David Pardo <davidpardomartin@gmail.com>
2024-01-29 21:49:24 +0100

    exercise done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../../styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+  const [lightStates, setLightStates] = useState({
+    roja: false,
+    amarilla: false,
+    verde: false,
+    purpura: false
+  });
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [currentColor, setCurrentColor] = useState('roja');
+
+  const handleLightClick = (clickedLight) => {
+    setLightStates({
+      roja: false,
+      amarilla: false,
+      verde: false,
+      purpura: false,
+      [clickedLight]: true
+    });
+    setCurrentColor(clickedLight);
+  };
+
+  const toggleColor = () => {
+    if (currentColor === 'roja') {
+      handleLightClick('verde');
+    } else if (currentColor === 'verde') {
+      handleLightClick('amarilla');
+    } else {
+      handleLightClick('roja');
+    }
+  };
+
+  const handleTrafficLightCoolClick = () => {
+ 
+    setLightStates({
+      roja: false,
+      amarilla: false,
+      verde: false,
+      purpura: true
+    });
+    setCurrentColor('purpura');
+  };
+
+  return (
+    <div>
+      <div id="barra"></div>
+      <div id="semaforo">
+        <div
+          className={`luz roja ${lightStates.roja && 'encendido'}`}
+          onClick={() => handleLightClick("roja")}
+        ></div>
+        <div
+          className={`luz amarilla ${lightStates.amarilla && 'encendido'}`}
+          onClick={() => handleLightClick("amarilla")}
+        ></div>
+        <div
+          className={`luz verde ${lightStates.verde && 'encendido'}`}
+          onClick={() => handleLightClick("verde")}
+        ></div>
+		
+      </div>
+      {lightStates.purpura && (
+        <div
+          className={`luz purpura ${lightStates.purpura && 'encendido'}`}
+        ></div>
+      )}
+      <div className='d-flex justify-content-center mt-5'>
+        <button className="btn btn-primary mt-3" onClick={toggleColor}>Change Color</button>
+        <button className="btn btn-info mt-3 ml-3 ms-2" onClick={handleTrafficLightCoolClick}>Traffic Light Cool</button>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default TrafficLight;
